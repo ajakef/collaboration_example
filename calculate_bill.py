f82be89c17c9612eb74e942d74114ee92e75ac44
Jake Anderson <ajakef@gmail.com>
2020-09-28 15:12:36 -0600

    added basic function

---
+++ calculate_bill.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def calculate_tip(price_list):
+    """
+    Calculate the total cost of a restaurant meal.
+    Parameters:
+    -----------
+    price_list : list of floats, prices of all foods/drinks ordered [dollars]
+
+    Returns:
+    --------
+    float, total cost of meal
+    """
+    tax_multiplier = 1.06
+    tip_multiplier = 1.15
+    return np.sum(price_list) * tax_multiplier * tip_multiplier
+
+
+def f(x):
+    return np.sum(x) * 1.06 * 1.15
