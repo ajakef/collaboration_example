93c9dcd45d5ff48a64272b192cab8983ba1eae2c
Jake Anderson <ajakef@gmail.com>
2020-09-28 15:39:29 -0600

    added the tax rate option

--- calculate_bill.py
+++ calculate_bill.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-def calculate_tip(price_list):
+def calculate_tip(price_list, tax_rate = 6):
     """
     Calculate the total cost of a restaurant meal.
     Parameters:
     -----------
     price_list : list of floats, prices of all foods/drinks ordered [dollars]
+    tax_rate
 
     Returns:
     --------
     float, total cost of meal
     """
-    tax_multiplier = 1.06
+    tax_multiplier = 1 + tax_rate/100
     tip_multiplier = 1.15
     return np.sum(price_list) * tax_multiplier * tip_multiplier
 
